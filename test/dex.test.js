dad2d3829d28d6daaa2dde9ea87d0bba22ad1a7e
AndrewProbert <andrewprobert15@gmail.com>
2024-06-12 21:23:22 -0400

    working swap and add liquidity/withdraw

--- test/dex.test.js
+++ test/dex.test.js
@@ -1,3 +1,11 @@
+let expect;
+
+before(async () => {
+    // Dynamically import chai
+    const chai = await import('chai');
+    expect = chai.expect;
+});
+
 const MyToken = artifacts.require("MyToken");
 const StableCoin = artifacts.require("StableCoin");
 const Dex = artifacts.require("Dex");
@@ -22,8 +30,8 @@ contract("Dex", accounts => {
         const reserve1 = await dex.reserve1();
         const reserve2 = await dex.reserve2();
 
-        assert.equal(reserve1.toString(), web3.utils.toWei('100', 'ether'));
-        assert.equal(reserve2.toString(), web3.utils.toWei('100', 'ether'));
+        expect(reserve1.toString()).to.equal(web3.utils.toWei('100', 'ether'));
+        expect(reserve2.toString()).to.equal(web3.utils.toWei('100', 'ether'));
     });
 
     it("should swap tokens", async () => {
@@ -35,17 +43,35 @@ contract("Dex", accounts => {
         const reserve1 = await dex.reserve1();
         const reserve2 = await dex.reserve2();
 
-        assert.equal(reserve1.toString(), web3.utils.toWei('110', 'ether'));
-        assert(reserve2.toString() < web3.utils.toWei('100', 'ether'));
+        expect(Number(reserve1.toString())).to.equal(Number(web3.utils.toWei('110', 'ether')));
+        expect(Number(reserve2.toString())).to.be.below(Number(web3.utils.toWei('100', 'ether')));
+
+        console.log("Swap Details:");
+        console.log("Amount In:", web3.utils.fromWei(amountIn, 'ether'), "MyToken");
+        console.log("Reserve 1:", web3.utils.fromWei(reserve1.toString(), 'ether'), "MyToken");
+        console.log("Reserve 2:", web3.utils.fromWei(reserve2.toString(), 'ether'), "StableCoin");
+        console.log("Price:", (reserve1 / reserve2).toString());
     });
 
     it("should remove liquidity", async () => {
+        const initialReserve1 = await dex.reserve1();
+        const initialReserve2 = await dex.reserve2();
+
         await dex.removeLiquidity(web3.utils.toWei('50', 'ether'), web3.utils.toWei('50', 'ether'), { from: accounts[0] });
 
         const reserve1 = await dex.reserve1();
         const reserve2 = await dex.reserve2();
 
-        assert.equal(reserve1.toString(), web3.utils.toWei('60', 'ether'));
-        assert.equal(reserve2.toString(), web3.utils.toWei('60', 'ether'));
+        expect(reserve1.toString()).to.equal(initialReserve1.sub(new web3.utils.BN(web3.utils.toWei('50', 'ether'))).toString());
+        expect(reserve2.toString()).to.equal(initialReserve2.sub(new web3.utils.BN(web3.utils.toWei('50', 'ether'))).toString());
+
+        console.log("Remove Liquidity Details:");
+        console.log("Initial Reserve 1:", web3.utils.fromWei(initialReserve1.toString(), 'ether'), "MyToken");
+        console.log("Initial Reserve 2:", web3.utils.fromWei(initialReserve2.toString(), 'ether'), "StableCoin");
+        console.log("Reserve 1:", web3.utils.fromWei(reserve1.toString(), 'ether'));
+        console.log("Reserve 2:", web3.utils.fromWei(reserve2.toString(), 'ether'));
+
+        
+
     });
 });
