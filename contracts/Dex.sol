2adea7ea76a3a200f8be7f49406928b150ce66fc
AndrewProbert <andrewprobert15@gmail.com>
2024-06-12 22:00:45 -0400

    added x * y = k method to the dex contract

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,60 +1,87 @@
-// contracts/Dex.sol
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Dex {
+contract Dex is Ownable(msg.sender) {
     IERC20 public token1;
     IERC20 public token2;
 
     uint256 public reserve1;
     uint256 public reserve2;
 
+    event LiquidityAdded(address indexed provider, uint256 amount1, uint256 amount2);
+    event LiquidityRemoved(address indexed provider, uint256 amount1, uint256 amount2);
+    event Swapped(address indexed swapper, address inputToken, uint256 inputAmount, address outputToken, uint256 outputAmount);
+
     constructor(address _token1, address _token2) {
         token1 = IERC20(_token1);
         token2 = IERC20(_token2);
     }
 
-    function addLiquidity(uint256 amount1, uint256 amount2) public {
+    function addLiquidity(uint256 amount1, uint256 amount2) external onlyOwner {
         token1.transferFrom(msg.sender, address(this), amount1);
         token2.transferFrom(msg.sender, address(this), amount2);
 
         reserve1 += amount1;
         reserve2 += amount2;
+
+        emit LiquidityAdded(msg.sender, amount1, amount2);
     }
 
-    function removeLiquidity(uint256 amount1, uint256 amount2) public {
-        require(reserve1 >= amount1, "Insufficient liquidity");
-        require(reserve2 >= amount2, "Insufficient liquidity");
+    function removeLiquidity(uint256 amount1, uint256 amount2) external onlyOwner {
+        require(reserve1 >= amount1, "Insufficient reserve1");
+        require(reserve2 >= amount2, "Insufficient reserve2");
 
         token1.transfer(msg.sender, amount1);
         token2.transfer(msg.sender, amount2);
 
         reserve1 -= amount1;
         reserve2 -= amount2;
+
+        emit LiquidityRemoved(msg.sender, amount1, amount2);
     }
 
-    function swap(address inputToken, uint256 inputAmount) public {
+    function swap(address inputToken, uint256 inputAmount) external {
         require(inputToken == address(token1) || inputToken == address(token2), "Invalid token");
 
-        bool isInputToken1 = inputToken == address(token1);
-        IERC20 input = isInputToken1 ? token1 : token2;
-        IERC20 output = isInputToken1 ? token2 : token1;
-
-        uint256 inputReserve = isInputToken1 ? reserve1 : reserve2;
-        uint256 outputReserve = isInputToken1 ? reserve2 : reserve1;
-
-        uint256 outputAmount = (inputAmount * outputReserve) / (inputReserve + inputAmount);
+        bool isToken1 = inputToken == address(token1);
+        (IERC20 input, IERC20 output, uint256 inputReserve, uint256 outputReserve) = isToken1 
+            ? (token1, token2, reserve1, reserve2) 
+            : (token2, token1, reserve2, reserve1);
 
         input.transferFrom(msg.sender, address(this), inputAmount);
+
+        uint256 inputAmountWithFee = inputAmount * 997; // Apply 0.3% fee
+        uint256 outputAmount = (inputAmountWithFee * outputReserve) / (inputReserve * 1000 + inputAmountWithFee);
+
+        require(outputAmount <= output.balanceOf(address(this)), "Insufficient liquidity for this trade");
+
         output.transfer(msg.sender, outputAmount);
 
-        if (isInputToken1) {
+        if (isToken1) {
             reserve1 += inputAmount;
             reserve2 -= outputAmount;
         } else {
             reserve2 += inputAmount;
             reserve1 -= outputAmount;
         }
+
+        emit Swapped(msg.sender, inputToken, inputAmount, address(output), outputAmount);
+    }
+
+    function getSpotPrice(address inputToken) external view returns (uint256) {
+        require(inputToken == address(token1) || inputToken == address(token2), "Invalid token");
+
+        if (inputToken == address(token1)) {
+            return (reserve2 * 1e18) / reserve1;
+        } else {
+            return (reserve1 * 1e18) / reserve2;
+        }
+    }
+
+    function getReserves() external view returns (uint256, uint256) {
+        return (reserve1, reserve2);
     }
 }
--- test/dex.test.js
+++ test/dex.test.js
@@ -22,70 +22,77 @@ contract("Dex", accounts => {
     });
 
     it("should add liquidity", async () => {
-        const initialReserve1 = await dex.reserve1();
-        const initialReserve2 = await dex.reserve2();
-        
         await myToken.approve(dex.address, web3.utils.toWei('100', 'ether'), { from: accounts[0] });
         await stableCoin.approve(dex.address, web3.utils.toWei('100', 'ether'), { from: accounts[0] });
 
+        console.log("Adding liquidity...");
         await dex.addLiquidity(web3.utils.toWei('100', 'ether'), web3.utils.toWei('100', 'ether'), { from: accounts[0] });
 
         const reserve1 = await dex.reserve1();
         const reserve2 = await dex.reserve2();
+        const spotPriceToken1Before = await dex.getSpotPrice(myToken.address);
+        const spotPriceToken2Before = await dex.getSpotPrice(stableCoin.address);
+
+        console.log("Reserve 1:", reserve1.toString());
+        console.log("Reserve 2:", reserve2.toString());
+        console.log("Spot Price Token 1 (Before):", web3.utils.fromWei(spotPriceToken1Before.toString(), 'ether'));
+        console.log("Spot Price Token 2 (Before):", web3.utils.fromWei(spotPriceToken2Before.toString(), 'ether'));
 
         expect(reserve1.toString()).to.equal(web3.utils.toWei('100', 'ether'));
         expect(reserve2.toString()).to.equal(web3.utils.toWei('100', 'ether'));
-
-        console.log("Add Liquidity Details:");
-        console.log("Initial Reserve 1:", web3.utils.fromWei(initialReserve1.toString(), 'ether'), "MyToken");
-        console.log("Initial Reserve 2:", web3.utils.fromWei(initialReserve2.toString(), 'ether'), "StableCoin");
-        console.log("Reserve 1:", web3.utils.fromWei(reserve1.toString(), 'ether'), "MyToken");
-        console.log("Reserve 2:", web3.utils.fromWei(reserve2.toString(), 'ether'), "StableCoin");
-        console.log("Price:", (reserve1 / reserve2).toString());
-
-
     });
 
     it("should swap tokens", async () => {
         const amountIn = web3.utils.toWei('10', 'ether');
         await myToken.approve(dex.address, amountIn, { from: accounts[0] });
 
+        console.log("Swapping tokens...");
         await dex.swap(myToken.address, amountIn, { from: accounts[0] });
 
         const reserve1 = await dex.reserve1();
         const reserve2 = await dex.reserve2();
+        const spotPriceToken1After = await dex.getSpotPrice(myToken.address);
+        const spotPriceToken2After = await dex.getSpotPrice(stableCoin.address);
+
+        console.log("Reserve 1:", reserve1.toString());
+        console.log("Reserve 2:", reserve2.toString());
+        console.log("Spot Price Token 1 (After):", web3.utils.fromWei(spotPriceToken1After.toString(), 'ether'));
+        console.log("Spot Price Token 2 (After):", web3.utils.fromWei(spotPriceToken2After.toString(), 'ether'));
 
         expect(Number(reserve1.toString())).to.equal(Number(web3.utils.toWei('110', 'ether')));
         expect(Number(reserve2.toString())).to.be.below(Number(web3.utils.toWei('100', 'ether')));
-
-        console.log("Swap Details:");
-        console.log("Amount In:", web3.utils.fromWei(amountIn, 'ether'), "MyToken");
-        console.log("Reserve 1:", web3.utils.fromWei(reserve1.toString(), 'ether'), "MyToken");
-        console.log("Reserve 2:", web3.utils.fromWei(reserve2.toString(), 'ether'), "StableCoin");
-        console.log("Price:", (reserve1 / reserve2).toString());
     });
+
+    it("should get spot price", async () => {
+        const spotPriceToken1 = await dex.getSpotPrice(myToken.address);
+        const spotPriceToken2 = await dex.getSpotPrice(stableCoin.address);
+
+        console.log("Spot Price Token 1:", web3.utils.fromWei(spotPriceToken1.toString(), 'ether'));
+        console.log("Spot Price Token 2:", web3.utils.fromWei(spotPriceToken2.toString(), 'ether'));
+
+        expect(Number(spotPriceToken1)).to.be.above(0);
+        expect(Number(spotPriceToken2)).to.be.above(0);
+    });
+
 
     it("should remove liquidity", async () => {
         const initialReserve1 = await dex.reserve1();
         const initialReserve2 = await dex.reserve2();
 
+        console.log("Removing liquidity...");
         await dex.removeLiquidity(web3.utils.toWei('50', 'ether'), web3.utils.toWei('50', 'ether'), { from: accounts[0] });
 
         const reserve1 = await dex.reserve1();
         const reserve2 = await dex.reserve2();
+        const spotPriceToken1Before = await dex.getSpotPrice(myToken.address);
+        const spotPriceToken2Before = await dex.getSpotPrice(stableCoin.address);
+
+        console.log("Reserve 1:", reserve1.toString());
+        console.log("Reserve 2:", reserve2.toString());
+        console.log("Spot Price Token 1 (Before):", web3.utils.fromWei(spotPriceToken1Before.toString(), 'ether'));
+        console.log("Spot Price Token 2 (Before):", web3.utils.fromWei(spotPriceToken2Before.toString(), 'ether'));
 
         expect(reserve1.toString()).to.equal(initialReserve1.sub(new web3.utils.BN(web3.utils.toWei('50', 'ether'))).toString());
         expect(reserve2.toString()).to.equal(initialReserve2.sub(new web3.utils.BN(web3.utils.toWei('50', 'ether'))).toString());
-
-        console.log("Remove Liquidity Details:");
-        console.log("Initial Reserve 1:", web3.utils.fromWei(initialReserve1.toString(), 'ether'), "MyToken");
-        console.log("Initial Reserve 2:", web3.utils.fromWei(initialReserve2.toString(), 'ether'), "StableCoin");
-        console.log("Reserve 1:", web3.utils.fromWei(reserve1.toString(), 'ether'));
-        console.log("Reserve 2:", web3.utils.fromWei(reserve2.toString(), 'ether'));
-        console.log("Price:", (reserve1 / reserve2).toString());
-
-
-
     });
-    
 });
