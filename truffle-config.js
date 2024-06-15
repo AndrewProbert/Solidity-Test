1acb55d9d21a0a596b85760976e95efb08601e0e
AndrewProbert <andrewprobert15@gmail.com>
2024-06-15 18:10:41 -0400

    new confi.js file

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,35 @@
 require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const util = require("util");
 
 const private_keys = [
   process.env.PRIVATE_KEY_0,
   process.env.PRIVATE_KEY_1,
 ];
 
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+class DelayedProvider {
+  constructor(provider, delayMs) {
+    this.provider = provider;
+    this.delayMs = delayMs;
+  }
+
+  send(...args) {
+    const sendAsync = util.promisify(this.provider.send).bind(this.provider);
+    return delay(this.delayMs).then(() => sendAsync(...args));
+  }
+}
+
 module.exports = {
   networks: {
     sepolia: {
-      provider: () => new HDWalletProvider({
+      provider: () => new DelayedProvider(new HDWalletProvider({
         privateKeys: private_keys,
         providerOrUrl: "https://sepolia.infura.io/v3/b931e338739a41d7a537c683bb2f21d5",
         numberOfAddresses: 2,
-      }),
+      }), 5000), // 5000 ms delay
       network_id: 11155111, // Modify the network id here
       gas: 5500000,
       confirmations: 2,
